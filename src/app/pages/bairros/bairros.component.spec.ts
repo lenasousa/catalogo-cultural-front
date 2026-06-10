65ed1c9ae091d6893860d88d802ef02b8439d517
Hln404 <helenasousasilva1@gmail.com>
2026-06-09 21:32:35 -0300

    refactor: adiciona componente Bairros com template e estilos

--- src/app/pages/bairros/bairros.component.spec.ts
+++ src/app/pages/bairros/bairros.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Bairros } from './bairros';
+import { Bairros } from './bairros.component';
 
 describe('Bairros', () => {
   let component: Bairros;
