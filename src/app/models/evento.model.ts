ed2684836c66fc69aae9c95148410e37c855d0c9
Hln404 <helenasousasilva1@gmail.com>
2026-06-09 21:22:09 -0300

    feat: define interface Evento com propriedades essenciais

--- src/app/models/evento.model.ts
+++ src/app/models/evento.model.ts
@@ -1,1 +1,15 @@
-export interface Evento {}
+export interface Evento {
+  id?: number;
+  nome: string;
+  tipo: string;
+  bairro: string;
+  endereco: string;
+  descricao: string;
+  zona: string;
+  gratuito: boolean;
+  valor: string;
+  data: string;
+  imagem?: string;
+  site?: string;
+  destaques?: string[];
+}
