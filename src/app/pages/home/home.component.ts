016a4eb53c5acf50fc3dc65a0db973d1b47ed891
Hln404 <helenasousasilva1@gmail.com>
2026-06-12 01:16:59 -0300

    fix: remove método carregarEventos e atualiza lógica de agrupamento por zona no componente Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router'; // Necessário para clicar e navegar
 import { EventoService } from '../../services/evento.service';
 import { Evento } from '../../models/evento.model';
-import { CardEventoComponent } from '../../components/card-evento/card-evento.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, CardEventoComponent],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  eventos: Evento[] = [];
+  zonasComEventos: { nome: string, quantidadeBairros: number }[] = [];
   private eventoService = inject(EventoService);
 
   ngOnInit(): void {
-    this.carregarEventos();
+    this.eventoService.getEventos().subscribe({
+      next: (eventos) => this.agruparPorZona(eventos)
+    });
   }
 
-  carregarEventos(): void {
-    this.eventoService.getEventos().subscribe({
-      next: (dados) => {
-        this.eventos = dados;
-        console.log('Eventos recebidos da API:', this.eventos);
-      },
-      error: (erro) => {
-        console.error('Erro ao comunicar com a API:', erro);
+  agruparPorZona(eventos: Evento[]): void {
+    const zonasMap = new Map<string, Set<string>>();
+
+    eventos.forEach(e => {
+      if (!zonasMap.has(e.zona)) {
+        zonasMap.set(e.zona, new Set<string>());
       }
+      zonasMap.get(e.zona)!.add(e.bairro);
     });
+
+    this.zonasComEventos = Array.from(zonasMap.entries()).map(([zona, bairros]) => ({
+      nome: zona,
+      quantidadeBairros: bairros.size
+    })).sort((a, b) => a.nome.localeCompare(b.nome));
   }
 }
