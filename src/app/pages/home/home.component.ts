b9566b538c633d775dfa46ea3e87a1c24ff438c8
Hln404 <helenasousasilva1@gmail.com>
2026-06-11 23:06:03 -0300

    feat: implementa lógica de carregamento de eventos na Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EventoService } from '../../services/evento.service'; // Confirma se o caminho está correto
+import { Evento } from '../../models/evento.model';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  eventos: Evento[] = [];
+  private eventoService = inject(EventoService);
+
+  ngOnInit(): void {
+    this.carregarEventos();
+  }
+
+  carregarEventos(): void {
+    this.eventoService.getEventos().subscribe({
+      next: (dados) => {
+        this.eventos = dados;
+        console.log('Eventos recebidos da API:', this.eventos);
+      },
+      error: (erro) => {
+        console.error('Erro ao comunicar com a API:', erro);
+      }
+    });
+  }
+}
