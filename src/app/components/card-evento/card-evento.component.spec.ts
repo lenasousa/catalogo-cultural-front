ca19916b3d76c88d7a44a1cc4ca36f9f4641ff24
Hln404 <helenasousasilva1@gmail.com>
2026-06-11 23:21:07 -0300

    refactor: renomeia componente CardEvento com template e estilos

--- src/app/components/card-evento/card-evento.component.spec.ts
+++ src/app/components/card-evento/card-evento.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CardEventoComponent } from './card-evento';
+import { CardEventoComponent } from './card-evento.component';
 
 describe('CardEventoComponent', () => {
   let component: CardEventoComponent;
