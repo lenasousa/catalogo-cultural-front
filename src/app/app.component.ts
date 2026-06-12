722aa380c7ed9d6f7d9720959eddb95fefeb0aad
Hln404 <helenasousasilva1@gmail.com>
2026-06-12 02:08:25 -0300

    fix: adiciona funcionalidade de rolagem para mostrar botão de voltar ao topo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ModalSugestaoComponent } from './components/modal-sugestao/modal-sugestao.component';
@@ -11,6 +11,16 @@ import { ModalSugestaoComponent } from './components/modal-sugestao/modal-sugest
 })
 export class AppComponent {
   statusModalSugestao: boolean = false;
+  mostrarBotaoTopo: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.mostrarBotaoTopo = window.scrollY > 300;
+  }
+
+  voltarAoTopo() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
   abrirSugestao(): void {
     this.statusModalSugestao = true;
