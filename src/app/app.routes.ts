9299f6f426a6ce507415df3c3ceb6e91c7d0e0a8
Hln404 <helenasousasilva1@gmail.com>
2026-06-11 22:16:59 -0300

    feat: adiciona rotas para os componentes Home, Eventos e Bairros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EventosComponent } from './pages/eventos/eventos.component';
+import { BairrosComponent } from './pages/bairros/bairros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Página inicial
+  { path: 'eventos', component: EventosComponent },
+  { path: 'bairros', component: BairrosComponent },
+  { path: '**', redirectTo: '' } // Rota de fallback contra URLs inválidas
+];
