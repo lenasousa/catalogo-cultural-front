5bdd1f37ba545012209bad2376932323cc633819
Hln404 <helenasousasilva1@gmail.com>
2026-06-11 22:35:28 -0300

    refactor: renomeia os os componetes app

--- src/app/app.component.config.ts
+++ src/app/app.component.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { routes } from './app.component.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
