62f103c83e16e01901c652b698012543f9fd5fc3
Hln404 <helenasousasilva1@gmail.com>
2026-06-12 03:17:12 -0300

    test: adiciona provedores de HTTP e roteamento no teste do HomeComponent

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,11 +10,16 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
-    }).compileComponents();
+      providers: [
+        provideHttpClient(),
+        provideRouter([])
+      ]
+    })
+    .compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
