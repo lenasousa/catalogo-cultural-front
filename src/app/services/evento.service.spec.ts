d6b9fbc52e95b1b8330da1062aa6e4ab2dff2b46
Hln404 <helenasousasilva1@gmail.com>
2026-06-12 03:16:01 -0300

    test: adiciona testes para o serviço de eventos com configuração de HTTP

--- src/app/services/evento.service.spec.ts
+++ src/app/services/evento.service.spec.ts
@@ -1,13 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { EventoService } from './evento.service';
 
 describe('EventoService', () => {
   let service: EventoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        EventoService,
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(EventoService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Garante que não há requisições pendentes
   });
 
   it('should be created', () => {
